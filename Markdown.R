256d42f438a7d2579ec1d7aa38f2ab54825aa1e4
AndresMPL <122495258+AndresMPL@users.noreply.github.com>
2023-04-23 19:34:49 -0500

    Intro
    Intro

---
+++ Markdown.R
@@ -0,0 +1,23 @@
+
+#----------------------------------------------------------------------------
+#
+# MARKDOWN
+# 4 de abril de 2023
+#
+#----------------------------------------------------------------------------
+
+#Introducción a Marksow: 
+#https://bookdown.org/gboccardo/manual-ED-UCH/introduccion-al-uso-de-rmarkdown-para-la-compilacion-de-resultados-de-rstudio-en-diferentes-formatos.html
+
+install.packages('knitr', dependencies = TRUE)
+tinytex::install_tinytex()
+
+# update all existing packages first
+update.packages(ask = FALSE, repos = 'http://cran.rstudio.org')
+install.packages('knitr', repos = c('http://rforge.net', 
+                                    'http://cran.rstudio.org'),
+                                     type = 'source')
+
+#Enlace elementos de markdown:
+#https://github.com/gboccardo/manual-ED-UCH
+
